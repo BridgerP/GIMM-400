922b933874c3ea945759da65438f0650cbec1e23
GTvsForza <christianeasterday@u.boisestate.edu>
2020-11-17 18:54:25 -0700

    Added Lap Timer, Lap Counter and Win Screen

---
+++ MLAgents/Assets/scripts/HalfPointTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour
+{
+    public GameObject lapCompleteTrig;
+    public GameObject halfLapTrig;
+
+    private void OnTriggerEnter()
+    {
+        lapCompleteTrig.SetActive(true);
+        halfLapTrig.SetActive(false);
+    }
+}
